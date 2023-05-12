c12c829e301532913679b6ba083bbd3ebbff0229
Albin Groen <albin.groen@gmail.com>
2023-05-12 19:10:10 +0200

    Update Treesitter config

--- init.lua
+++ init.lua
@@ -108,13 +108,11 @@ vim.o.background = 'dark'
 
 require'nvim-treesitter.configs'.setup {
   ensure_installed = { "typescript" },
-  ignore_install = { "phpdoc" },
   context_commentstring = {
     enable = true
   },
   highlight = {
-    enable = true,
-    disable = { "lua" }
+    enable = true
   },
   indent = {
     enable = true
