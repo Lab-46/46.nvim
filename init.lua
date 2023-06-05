ed40905aff282a1da9d4cfdad720e7803e998a4f
Albin Groen <albin.groen@gmail.com>
2023-06-05 16:21:24 +0200

    Refactor code

--- init.lua
+++ init.lua
@@ -4,7 +4,7 @@
 -- | |_) | (_| \__ \ | (__\__ \
 -- |____/ \__,_|___/_|\___|___/
 
-vim.g.mapleader = " "
+vim.g.mapleader = ' '
 vim.o.colorcolumn = ''
 vim.o.cursorline = true
 vim.o.errorbells = false
@@ -27,20 +27,28 @@ vim.o.swapfile = false
 vim.o.tabstop = 2
 vim.o.termguicolors = true
 vim.o.wrap = false
-vim.opt.fillchars = { eob = " " }
+vim.opt.fillchars = { eob = ' ' }
+vim.o.hidden = true
+vim.o.backup = false
+vim.o.writebackup = false
+vim.o.updatetime = 300
+vim.opt.list = true
+vim.opt.listchars:append 'space:⋅'
 
-vim.api.nvim_set_keymap("n", "<leader>c", ":CocRestart<CR><CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<C-H>', '<C-W><C-H>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<C-J>', '<C-W><C-J>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<C-K>', '<C-W><C-K>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<C-L>', '<C-W><C-L>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<C-S>', ':%s/', { noremap = true })
-vim.api.nvim_set_keymap('n', 'sp', ':sp<CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', 'tj', ':tabprev<CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', 'tk', ':tabnext<CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', 'tn', ':tabnew<CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', 'to', ':tabo<CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', 'vs', ':vs<CR>', { noremap = true })
+local map = vim.api.nvim_set_keymap
+
+map('n', '<leader>c', ':CocRestart<CR><CR>', { noremap = true, silent = true })
+map('n', '<C-H>', '<C-W><C-H>', { noremap = true })
+map('n', '<C-J>', '<C-W><C-J>', { noremap = true })
+map('n', '<C-K>', '<C-W><C-K>', { noremap = true })
+map('n', '<C-L>', '<C-W><C-L>', { noremap = true })
+map('n', '<C-S>', ':%s/', { noremap = true })
+map('n', 'sp', ':sp<CR>', { noremap = true })
+map('n', 'tj', ':tabprev<CR>', { noremap = true })
+map('n', 'tk', ':tabnext<CR>', { noremap = true })
+map('n', 'tn', ':tabnew<CR>', { noremap = true })
+map('n', 'to', ':tabo<CR>', { noremap = true })
+map('n', 'vs', ':vs<CR>', { noremap = true })
 
 --  ____  _             _
 -- |  _ \| |_   _  __ _(_)_ __  ___
@@ -49,49 +57,54 @@ vim.api.nvim_set_keymap('n', 'vs', ':vs<CR>', { noremap = true })
 -- |_|   |_|\__,_|\__, |_|_| |_|___/
 --                |___/
 
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable',
     lazypath,
   })
 end
 
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({
+require('lazy').setup({
   'nvim-treesitter/nvim-treesitter',
   'tpope/vim-commentary',
   { 'neoclide/coc.nvim',   branch = 'release' },
-  { 'kvrohit/rasmus.nvim', priority = 200 },
+  {
+    'kvrohit/rasmus.nvim',
+    lazy = false,
+    priority = 1000
+  },
   {
     'JoosepAlviste/nvim-ts-context-commentstring',
-    event = "VeryLazy"
+    event = 'VeryLazy'
   },
   {
     'nvim-telescope/telescope.nvim',
     dependencies = { { 'nvim-lua/plenary.nvim' } },
-    event = "VeryLazy",
+    event = 'VeryLazy',
   },
   {
-    "kylechui/nvim-surround",
-    version = "*",
-    event = "VeryLazy",
+    'kylechui/nvim-surround',
+    version = '*',
+    event = 'VeryLazy',
     config = function()
-      require("nvim-surround").setup()
+      require('nvim-surround').setup()
     end
   },
   {
-    "nvim-tree/nvim-tree.lua",
-    version = "*",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    event = "VeryLazy"
+    'nvim-tree/nvim-tree.lua',
+    version = '*',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    event = 'VeryLazy'
   },
+  'lukas-reineke/indent-blankline.nvim',
 })
 
 --    ____      _
@@ -100,8 +113,7 @@ require("lazy").setup({
 --  | |__| (_) | | (_) | |  \__ \
 --   \____\___/|_|\___/|_|  |___/
 
-vim.g.rasmus_transparent = true
-vim.cmd.colors("rasmus")
+vim.cmd.colors('rasmus')
 vim.o.background = 'dark'
 
 --   _____                   _ _   _
@@ -111,7 +123,7 @@ vim.o.background = 'dark'
 --    |_||_|  \___|\___||___/_|\__|\__\___|_|
 
 require 'nvim-treesitter.configs'.setup {
-  ensure_installed = { "typescript" },
+  ensure_installed = { 'typescript' },
   context_commentstring = {
     enable = true
   },
@@ -128,22 +140,16 @@ require 'nvim-treesitter.configs'.setup {
 --  | (_| (_) | (__
 --   \___\___/ \___|
 
-vim.api.nvim_set_keymap("n", "<leader>.", "<Plug>(coc-codeaction)", {})
-vim.api.nvim_set_keymap("n", "gd", "<Plug>(coc-definition)", { silent = true })
-vim.api.nvim_set_keymap("n", "gr", "<Plug>(coc-references)", { silent = true })
-vim.api.nvim_set_keymap("n", "K", ":call CocActionAsync('doHover')<CR>", { silent = true, noremap = true })
-vim.api.nvim_set_keymap("n", "<leader>rn", "<Plug>(coc-rename)", {})
-vim.api.nvim_set_keymap("n", "<leader>f", ":CocCommand prettier.formatFile<CR>", { noremap = true })
-vim.api.nvim_set_keymap("i", "<C-Space>", "coc#refresh()", { silent = true, expr = true })
-vim.api.nvim_set_keymap("i", "<TAB>", "coc#pum#visible() ? coc#pum#next(1) : '<TAB>'",
-  { noremap = true, silent = true, expr = true })
-vim.api.nvim_set_keymap("i", "<S-TAB>", "coc#pum#visible() ? coc#pum#prev(1) : '<C-h>'", { noremap = true, expr = true })
-vim.api.nvim_set_keymap("i", "<CR>", "coc#pum#visible() ? coc#pum#confirm() : '<C-G>u<CR><C-R>=coc#on_enter()<CR>'",
-  { silent = true, expr = true, noremap = true })
-vim.o.hidden = true
-vim.o.backup = false
-vim.o.writebackup = false
-vim.o.updatetime = 300
+map('n', '<leader>.', '<Plug>(coc-codeaction)', {})
+map('n', 'gd', '<Plug>(coc-definition)', { silent = true })
+map('n', 'gr', '<Plug>(coc-references)', { silent = true })
+map('n', 'K', ":call CocActionAsync('doHover')<CR>", { silent = true, noremap = true })
+map('n', '<leader>rn', '<Plug>(coc-rename)', {})
+map('n', '<leader>f', ':CocCommand prettier.formatFile<CR>', { noremap = true })
+map('i', '<C-Space>', 'coc#refresh()', { silent = true, expr = true })
+map('i', '<TAB>', "coc#pum#visible() ? coc#pum#next(1) : '<TAB>'", { noremap = true, silent = true, expr = true })
+map('i', '<S-TAB>', "coc#pum#visible() ? coc#pum#prev(1) : '<C-h>'", { noremap = true, expr = true })
+map('i', '<CR>', "coc#pum#visible() ? coc#pum#confirm() : '<C-G>u<CR><C-R>=coc#on_enter()<CR>'", { silent = true, expr = true, noremap = true })
 
 --   _____    _
 --  |_   _|__| | ___  ___  ___ ___  _ __   ___
@@ -152,19 +158,25 @@ vim.o.updatetime = 300
 --    |_|\___|_|\___||___/\___\___/| .__/ \___|
 --                                 |_|
 
-local telescope = require("telescope")
+local telescope = require('telescope')
+local telescope_builtin = require('telescope.builtin')
 
 telescope.setup {
   pickers = {
     find_files = {
-      theme = "dropdown",
+      theme = 'dropdown',
+      previewer = false,
+    },
+    buffers = {
+      theme = 'dropdown',
       previewer = false,
     }
   },
 }
 
-vim.api.nvim_set_keymap('n', '<C-P>', "<cmd>lua require('telescope.builtin').find_files()<CR>", { noremap = true })
-vim.api.nvim_set_keymap('n', '<C-F>', "<cmd>lua require('telescope.builtin').live_grep()<CR>", { noremap = true })
+vim.keymap.set('n', '<C-P>', telescope_builtin.find_files, {})
+vim.keymap.set('n', '<C-F>', telescope_builtin.live_grep, {})
+vim.keymap.set('n', '<C-B>', telescope_builtin.buffers, {})
 
 --              _                 _
 --   _ ____   _(_)_ __ ___       | |_ _ __ ___  ___
@@ -172,7 +184,7 @@ vim.api.nvim_set_keymap('n', '<C-F>', "<cmd>lua require('telescope.builtin').liv
 --  | | | \ V /| | | | | | |_____| |_| | |  __/  __/
 --  |_| |_|\_/ |_|_| |_| |_|      \__|_|  \___|\___|
 
-local api = require("nvim-tree.api")
+local api = require('nvim-tree.api')
 
 local function edit_or_open()
   local node = api.tree.get_node_under_cursor()
@@ -185,10 +197,10 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
 -- global
-vim.api.nvim_set_keymap("n", "<C-n>", ":NvimTreeFindFileToggle<cr>", { silent = true, noremap = true })
+map('n', '<C-n>', ':NvimTreeFindFileToggle<cr>', { silent = true, noremap = true })
 
 -- empty setup using defaults
-require("nvim-tree").setup {
+require('nvim-tree').setup {
   git = {
     enable = true
   },
@@ -206,6 +218,16 @@ require("nvim-tree").setup {
     ignore_list = {}
   },
   filters = {
-    dotfiles = true,
-  },
+    dotfiles = true
+  }
+}
+
+--   _           _            _        _     _             _    _ _                         _           
+--  (_)_ __   __| | ___ _ __ | |_     | |__ | | __ _ _ __ | | _| (_)_ __   ___   _ ____   _(_)_ __ ___  
+--  | | '_ \ / _` |/ _ \ '_ \| __|____| '_ \| |/ _` | '_ \| |/ / | | '_ \ / _ \ | '_ \ \ / / | '_ ` _ \ 
+--  | | | | | (_| |  __/ | | | ||_____| |_) | | (_| | | | |   <| | | | | |  __/_| | | \ V /| | | | | | |
+--  |_|_| |_|\__,_|\___|_| |_|\__|    |_.__/|_|\__,_|_| |_|_|\_\_|_|_| |_|\___(_)_| |_|\_/ |_|_| |_| |_|
+
+require('indent_blankline').setup {
+  space_char_blankline = ' ',
 }
